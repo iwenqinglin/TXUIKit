83114884ecc9fe1b6bae6ea3a78120f45f6b59a4
iwenqinglin <578622889@qq.com>
2020-05-15 23:28:12 +0800

    add new file

---
+++ TXUIKit/Classes/TXSearchBar.h
@@ -0,0 +1,12 @@
+//  TXSearchBar.h
+//  TXUIKitPro
+//
+//  Created by 赵小东 on 2020/5/15.
+//  Copyright © 赵小东 (QQ:1530591908) All rights reserved.
+
+
+#import <UIKit/UIKit.h>
+
+@interface TXSearchBar : UIView
+
+@end
---
+++ TXUIKit/Classes/TXSearchBar.m
@@ -0,0 +1,20 @@
+//  TXSearchBar.m
+//  TXUIKitPro
+//
+//  Created by 赵小东 on 2020/5/15.
+//  Copyright © 赵小东 (QQ:1530591908) All rights reserved.
+
+
+#import "TXSearchBar.h"
+
+@implementation TXSearchBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
